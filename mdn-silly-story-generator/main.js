18cbaeb601934128409a56cd9a0782921e572b19
Hepran Putra <hepranputra@gmail.com>
2024-02-24 23:04:19 +0700

    refactor: silly story generator

--- mdn-silly-story-generator/main.js
+++ mdn-silly-story-generator/main.js
@@ -1,60 +1,48 @@
-const customName = document.getElementById("customname");
-const randomize = document.querySelector(".randomize");
-const story = document.querySelector(".story");
-const body = document.querySelector("body");
+const customNameInput = document.getElementById("customname-input");
+const randomizeBtn = document.querySelector(".randomize-btn");
+const outputParagraph = document.querySelector(".output-paragraph");
 
 function randomValueFromArray(array) {
-  const random = Math.floor(Math.random() * array.length);
-  return array[random];
+    const random = Math.floor(Math.random() * array.length);
+    return array[random];
 }
 
-// Let it be one line long. Repot kalo mengubahnya jadi beberapa baris.
-// Dan tak ada manfaat yang signifikan dari hal tsb.
-// I quoted the replacable substring to make me easy to spot them.
-let storyText = "It was 94 fahrenheit outside, so ':insertx:' went for a walk. When they got to ':inserty:', they stared in horror for a few moments, then ':insertz:'. Bob saw the whole thing, but was not surprised — ':insertx:' weighs 300 pounds, and it was a hot day.";
+const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
-let insertX = [
-  "Willy the Goblin", "Big Daddy", "Father Christmas",
-];
+let insertX = ["Willy the Goblin", "Big Daddy", "Father Chrismas"];
 
-let insertY = [
-  "the soup kitchen", "Disneyland", "the White House",
-];
+let insertY = ["the soup kitchen", "Disneyland", "the White House"];
 
-let insertZ = [
-  "spontaneously combusted",
+let insertZ = ["spontaneously combusted", 
   "melted into a puddle on the sidewalk",
   "turned into a slug and crawled away",
 ];
 
-randomize.addEventListener("click", result);
+randomizeBtn.addEventListener("click", result);
 
 function result() {
-  let newStory = storyText;
+    let newStory = storyText;
 
-  let xItem = randomValueFromArray(insertX);
-  let yItem = randomValueFromArray(insertY);
-  let zItem = randomValueFromArray(insertZ);  
+    let xItem = randomValueFromArray(insertX);
+    let yItem = randomValueFromArray(insertY);
+    let zItem = randomValueFromArray(insertZ);
 
-  if (customName.value !== "") {
-    const name = customName.value;
-    newStory = newStory.replace("Bob", name);
-  }
+    newStory = newStory.replaceAll(":insertx:", xItem);
+    newStory = newStory.replace(":inserty:", yItem);
+    newStory = newStory.replace(":insertz:", zItem);
 
-  newStory = newStory.replaceAll(":insertx:", xItem);
-  newStory = newStory.replace(":inserty:", yItem);
-  newStory = newStory.replace(":insertz:", zItem);
+    if (customNameInput.value !== "") {
+        const name = customNameInput.value;
+        newStory = newStory.replace("Bob", name);
+    }
 
-  if (document.getElementById("uk").checked) {
-    // pounds -> stone          : stone = pound / 14
-    // fahrenheit -> centigrade : centigrade = 5 / 9 * (fahrenheit - 32);
-    const weight = Math.round(300 / 14) + " stone";
-    const temperature = Math.round(5 / 9 * (94 - 32)) + " centigrade";
+    if (document.getElementById("uk").checked) {
+        const weight = Math.round(300 / 14) + " stone";
+        const temp = Math.round(5 / 9 * (94 - 32)) + " centigrade";
+        newStory = newStory.replace("300 pounds", weight);
+        newStory = newStory.replace("94 fahrenheit", temp);
+    }
 
-    newStory = newStory.replace("94 fahrenheit", temperature);
-    newStory = newStory.replace("300 pounds", weight);
-  }
-
-  story.textContent = newStory;
-  story.style.visibility = "visible";
+    outputParagraph.textContent = newStory;
+    outputParagraph.style.visibility = "visible";
 }
